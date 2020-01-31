fc24b8d87ff00692ce5763b142d25245ca400dc5
Zach Mommaerts <zmommaerts@gmail.com>
2020-01-31 11:20:51 -0600

    added all classes

---
+++ SweepstakesProject/SweepstakesQueueManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    class SweepstakesQueueManager
+    {
+        //Member Variables (HAS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
